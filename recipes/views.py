9f4f32239878af6abe0a4bddb1e2d6de3d86cd83
Ankiwoong <ankiwoong@gmail.com>
2020-06-18 11:14:08 +0900

    Views

--- recipes/views.py
+++ recipes/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.views.generic import ListView, DetailView
 
-# Create your views here.
+from .models import Recipe
+
+
+class RecipeListView(ListView):
+    model = Recipe
+    template_name = "recipe_list.html"
+
+
+class RecipeDetailView(DetailView):
+    model = Recipe
+    template_name = "recipe_detail.html"
+
